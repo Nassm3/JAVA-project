9c1b7ac3cd42595f0014b7c9e6d297a5917815e9
RACOUPEAU Martin <nitram31300@gmail.com>
2022-04-22 11:05:04 +0200

    added bunch of stuff

--- BatailleNavale.java
+++ BatailleNavale.java
@@ -7,6 +7,16 @@ public class BatailleNavale implements ActionListener{
     int tour;
     InterfaceBatailleNavalle ib ;
     public BatailleNavale(){
+        p = new Plateau();
+        joueurs = new Joueur[2];
+        ib = new InterfaceBatailleNavalle(this);
+
+        for (int i = 0; i < joueurs.length; i++){
+            System.out.println("---joueur" + String.valueOf(i+1));
+            joueurs[i] = new Joueur(false, i+1, im);
+        }
+        Random r = new Random();
+        tour = r.nextInt(2);
 
     }
     
--- InterfaceBatailleNavalle.java
+++ InterfaceBatailleNavalle.java
@@ -1,16 +1,12 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridLayout;
-
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
-public class InterfaceBatailleNavalle {
+public class InterfaceBatailleNavalle implements ActionListener{
 	
-	public InterfaceBatailleNavalle() {
+	public InterfaceBatailleNavalle(int size) {
 		
-		JFrame f= new JFrame();
+		JFrame f = new JFrame();
 		f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		f.setLayout(new BorderLayout());
 		
@@ -21,8 +17,8 @@ public class InterfaceBatailleNavalle {
 		JPanel plateau =new JPanel(new GridLayout(1,2,50,50)); 
 		
 		
-		JPanel p1=new JPanel(new GridLayout(10,10));
-		JButton [][] b1=new JButton[10][10];
+		JPanel p1=new JPanel(new GridLayout(size,size));
+		JButton [][] b1=new JButton[size][size];
 		
 		for (int i=0;i<b1.length;i++)
 			for (int j=0;j<b1[i].length;j++) {
@@ -33,8 +29,8 @@ public class InterfaceBatailleNavalle {
 			}
 		plateau.add(p1);
 		
-		JPanel p2=new JPanel(new GridLayout(10,10));
-		JButton [][] b2=new JButton[10][10];
+		JPanel p2=new JPanel(new GridLayout(size,size));
+		JButton [][] b2=new JButton[size][size];
 		
 		for (int i=0;i<b2.length;i++)
 			for (int j=0;j<b2[i].length;j++) {
@@ -47,6 +43,11 @@ public class InterfaceBatailleNavalle {
 		
 		JLabel res = new JLabel("choisir une position");
 		res.setPreferredSize(new Dimension(25,25));
+
+		
+
+		
+		
 		
 		f.setLayout(new BorderLayout());
 		f.add(tour, BorderLayout.PAGE_START);
@@ -55,12 +56,64 @@ public class InterfaceBatailleNavalle {
 		f.pack();
 		f.setLocationRelativeTo(null);
 		f.setVisible(true);
+
+	}
+
+	public void choosePanel(){ 
+
+
+		JFrame frame = new JFrame("Paramètre de jeu");
+
+		JPanel panelPseudo = new JPanel(new GridLayout(1, 2));
+			JLabel pseudo = new JLabel("Saisir votre pseudo :");
+			JTextField pseudoField = new JTextField(20);
+			panelPseudo.add(pseudo);
+			panelPseudo.add(pseudoField);
+
+		JPanel panelChoixDifficulty = new JPanel(new GridLayout(2, 1));
+			JPanel parameterDifficulty = new JPanel(new FlowLayout(FlowLayout.LEFT));
+			String E[] = { "Facile", "Normale"};
+			JComboBox<String> combobox = new JComboBox<String>(E);
+			JLabel l1 = new JLabel("choisissez la difficulté de l'ordinateur");
+			parameterDifficulty.add(l1);
+			parameterDifficulty.add(combobox);
+			panelChoixDifficulty.add(parameterDifficulty);
+
+		JPanel panelChoixSize = new JPanel(new GridLayout(2, 1));
+			JPanel parameterSize = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+			String E1[] = { "10", "20", "30", "40", "50", "60"};
+			JComboBox<String> comboboxSize = new JComboBox<String>(E1);
+			JLabel l2 = new JLabel("choisissez la taille du plateau");
+			parameterSize.add(l2);
+			parameterSize.add(comboboxSize);
+			panelChoixSize.add(parameterSize);
+
+
+
+		frame.add(panelPseudo, BorderLayout.LINE_START);
+		frame.add(panelChoixDifficulty, BorderLayout.WEST);
+		frame.add(panelChoixSize, BorderLayout.CENTER);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+		
+
+
+
+		JButton jouer = new JButton("Jouer");
+
+
+
 	}
 	
-	
-	
 	public static void main(String[] args) {
-		new InterfaceBatailleNavalle();
+		new InterfaceBatailleNavalle(10).choosePanel();;
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		System.out.println(e);
+		
 	}
     
 }
