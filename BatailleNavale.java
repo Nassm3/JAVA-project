43257aa2080c6182f8b038f7985013fd0381fc1b
RACOUPEAU Martin <nitram31300@gmail.com>
2022-04-22 08:59:50 +0200

    yey

--- BatailleNavale.java
+++ BatailleNavale.java
@@ -1,3 +1,16 @@
-public class BatailleNavale {
+import java.awt.event.*;
+
+
+public class BatailleNavale implements ActionListener{
+    Plateau p;
+    Joueur[] joueurs;
+    int tour;
+    InterfaceBatailleNavalle ib ;
+    public BatailleNavale(){
+
+    }
     
+    public void actionPerformed(ActionEvent e) {
+        System.out.println(e);
+    }
 }
