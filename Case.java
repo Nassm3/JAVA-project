c5057a9fa0f1a16d70b5d0be290febcfd96860dc
nassim dup <nd202000@gmail.com>
2022-04-22 09:14:34 +0200

    case

--- Case.java
+++ Case.java
@@ -1,3 +1,14 @@
-public class Case{
-    
+ 
+public enum Case {
+	SUNK("X"),
+	EMPTY(" "),
+	BOAT("B"),
+	HIT("H");
+	
+	private String affichage;
+	private Case(String aff) {
+		affichage=aff;
+	}
+	public String affiche() {return affichage;}
+
 }
